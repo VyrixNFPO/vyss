b8e6bd953fc4e2ba36d8f54f211390dedf03bcfc
VyrixNFPO <132283906+VyrixNFPO@users.noreply.github.com>
2023-05-03 18:32:42 +0000

    New: colors/background, vyss (javascript) Update: vyss (CSS)

---
+++ vyss.js
@@ -0,0 +1,9 @@
+var head = document.getElementsByTagName('HEAD')[0];
+
+var link = document.createElement('link');
+
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'vyss.css';
+
+head.appendChild(link);
